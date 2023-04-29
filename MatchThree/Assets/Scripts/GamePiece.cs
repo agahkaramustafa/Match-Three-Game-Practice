3a08b8e08035d363ada3cda9d1bac2166cd35bc1
Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
2023-04-29 19:22:31 +0300

    Gamepiece Movement

--- MatchThree/Assets/Scripts/GamePiece.cs
+++ MatchThree/Assets/Scripts/GamePiece.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class GamePiece : MonoBehaviour
 {
     public int xIndex;
     public int yIndex;
+
+    bool m_isMoving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,15 @@ public class GamePiece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move((int)transform.position.x + 1, (int)transform.position.y, 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move((int)transform.position.x - 1, (int)transform.position.y, 0.5f);
+        }
     }
 
     public void SetCoord(int x, int y)
@@ -24,4 +35,52 @@ public class GamePiece : MonoBehaviour
 		xIndex = x;
 		yIndex = y;
 	}
+
+    public void Move(int destX, int destY, float timeToMove)
+	{
+        if (!m_isMoving)
+        {
+            StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), timeToMove));
+        }
+		
+	}
+
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+	{
+		Vector3 startPosition = transform.position;
+
+		bool reachedDestination = false;
+
+        float elapsedTime = 0f;
+
+        m_isMoving = true;
+
+        while (!reachedDestination)
+		{
+            // if we are close enough to destination
+            if (Vector3.Distance(transform.position, destination) < Mathf.Epsilon)
+            {
+                reachedDestination = true;
+                transform.position = destination;
+                SetCoord((int)destination.x, (int)destination.y);
+                break;
+            }
+
+            // track the total running time
+            elapsedTime += Time.deltaTime;
+
+            // calculate the Lerp value
+            float t = Mathf.Clamp(elapsedTime / timeToMove, 0, 1);
+
+            // move the game piece
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+            // wait until next frame
+            yield return null;
+        }
+
+        m_isMoving = false;
+	}
 }
+	
+
