528436ed45bad55bdfb693272169a042ec8e7571
Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
2023-04-29 16:57:10 +0300

    Revert "Tiles"
    This reverts commit b5fe3f471cb059a43553ab590e35447cdff410ec.

--- MatchThree/Assets/Scripts/Tile.cs
+++ MatchThree/Assets/Scripts/Tile.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public int xIndex;
-    public int yIndex;
-
-    Board m_board;
-
-    public void Init(int x, int y, Board board)
+    // Start is called before the first frame update
+    void Start()
     {
-        xIndex = x;
-        yIndex = y;
-        m_board = board;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
